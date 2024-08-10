55a2ed3c40f17ba5aa2b07cf5d62592afcb541b0
vishva310 <bca2022vishva1745@tnraocollege.org>
2024-08-10 18:06:34 +0530

    Add files via upload

---
+++ pro2.py
@@ -0,0 +1,14 @@
+emp = {
+    'id': 101,
+    'name': 'John Doe',
+    'designation': 'Software Engineer',
+    'salary': 70000
+}
+del emp['designation']
+
+emp['name'] = 'Jane Doe'
+
+print(emp)
+
+
+
